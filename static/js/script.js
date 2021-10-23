1b9db3016f227d07b456f9660bca82103b00ffbe
priyanka prasad <priyankaafssulur@gmail.com>
2021-10-23 11:50:22 +0530

    ✨ Getting started

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+function ageInDays(){
+    var birthYear = prompt("What year were you born ...?");
+    var ageInDays = (2021-birthYear)*365;
+    var h1 = document.createElement('h1');
+   
+    var textanswer = document.createTextNode('You are' + ageInDays + 'days old');
+    h1.setAttribute('id','ageinDays');
+    h1.appendChild(textanswer);
+    document.getElementById('result').appendChild(h1);
+    console.log(ageInDays);
+}
